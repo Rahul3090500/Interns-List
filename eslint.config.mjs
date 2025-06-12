831b855de6e3508254205fc5834a9f3d1f91e91b
kurageGit <109056697+Rahul3090500@users.noreply.github.com>
2025-06-13 02:29:54 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,29 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "react/prop-types": "off",
-      "@next/next/no-img-element": "off",
-      "react-hooks/exhaustive-deps": "off",
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-    overrides: [
-      {
-        files: ["*.ts", "*.tsx"],
-        rules: {
-          "@typescript-eslint/explicit-module-boundary-types": "off", // Disable for TS files
-        },
-      },
-    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off', 
+    },
   },
-];
-
-export default eslintConfig;
+)
